b89cd87b3d64a19379ed1bb9026b6cd4f4602206
Peter <peter.merseburger@uni-jena.de>
2019-09-03 13:48:43 +0200

    Get autowiring of repository class working

--- src/main/kotlin/de/unijena/cheminf/coconutfrontend/CoconutfrontendApplication.kt
+++ src/main/kotlin/de/unijena/cheminf/coconutfrontend/CoconutfrontendApplication.kt
@@ -2,8 +2,12 @@ package de.unijena.cheminf.coconutfrontend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@Configuration
+@EnableMongoRepositories("de.unijena.cheminf.npopensourcecollector.mongocollections")
 class CoconuTfrontendApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/de/unijena/cheminf/coconutfrontend/controller/IndexController.kt
+++ src/main/kotlin/de/unijena/cheminf/coconutfrontend/controller/IndexController.kt
@@ -1,13 +1,18 @@
 package de.unijena.cheminf.coconutfrontend.controller
 
+import de.unijena.cheminf.npopensourcecollector.mongocollections.UniqueNaturalProductRepository
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class IndexController {
+class IndexController(var repository: UniqueNaturalProductRepository) {
+
 
     @RequestMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
+        model["unps"] = this.repository.findByInchikey("NVHVRPXPHFEYKX-UHFFFAOYSA-N")
         return "index"
     }
 
