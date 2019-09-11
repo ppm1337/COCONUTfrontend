2253d9bd7a52c2baff72c68ea67fdf5b79f4a67e
Peter <peter.merseburger@uni-jena.de>
2019-09-11 17:18:15 +0200

    Render structure also in TableBrowser

--- src/main/js/components/NaturalProductTableItem.js
+++ src/main/js/components/NaturalProductTableItem.js
@@ -1,5 +1,8 @@
+import Image from "react-bootstrap/Image";
+
+const OpenChemLib = require("openchemlib/full");
 const React = require("react");
-const ReactDOM = require("react-dom");
+
 
 class NaturalProductTableItem extends React.Component {
     constructor(props) {
@@ -7,8 +10,16 @@ class NaturalProductTableItem extends React.Component {
     }
 
     render() {
+        let canvas = document.createElement("canvas");
+        canvas.width = 200;
+        canvas.height = 100;
+
+        const npMolecule = OpenChemLib.Molecule.fromSmiles(this.props.naturalProduct.smiles);
+
+        OpenChemLib.StructureView.drawMolecule(canvas, npMolecule);
+
         return <tr>
-            <td>not implemented yet</td>
+            <td><Image src={canvas.toDataURL()} alt="🥥"/></td>
             <td>not implemented yet</td>
             <td>{this.props.naturalProduct.npl_score}</td>
             <td>{this.props.naturalProduct.molecular_formula}</td>
