0542d5a16095335eefcb2ba2e6f46d7881f98d4b
Peter <peter.merseburger@uni-jena.de>
2019-09-12 12:28:37 +0200

    Improve NaturalProductCompoundCard component dummy

--- src/main/js/components/NaturalProductCompoundCard.js
+++ src/main/js/components/NaturalProductCompoundCard.js
@@ -2,6 +2,8 @@ import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Table from "react-bootstrap/Table";
 
 const React = require("react");
 
@@ -9,23 +11,156 @@ const React = require("react");
 class NaturalProductCompoundCard extends React.Component {
     render() {
         return <Container>
-            <Col sm={2}>
-                <Nav defaultActiveKey="" className="flex-column">
-                    <Nav.Link href="">placeholder</Nav.Link>
-                    <Nav.Link eventKey="">placeholder</Nav.Link>
-                </Nav>
-            </Col>
-            <Col sm={10}>
-                <Row>
-                    <h2>Representations</h2>
-                </Row>
-                <Row>
-                    <h2>Computed properties</h2>
-                </Row>
-                <Row>
-                    <h2>Species</h2>
-                </Row>
-            </Col>
+            <Row>
+                <Col sm={2}>
+                    <Nav defaultActiveKey="" className="flex-column" classID="compoundCardSidebar">
+                        <Nav.Link className="text-secondary" href="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                        <Nav.Link className="text-secondary" eventKey="">placeholder</Nav.Link>
+                    </Nav>
+                </Col>
+                <Col sm={10}>
+                    <Row>
+                        <h2>Name goes here</h2>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <Col sm={4}>
+                            <Image src="" alt=""/>
+                        </Col>
+                        <Col sm={8}>
+                            <h4></h4>
+                            <Table>
+                                <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <h4>Representations</h4>
+                        <Table>
+                            <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Row>
+                    <Row>
+                        <h4>Computed properties</h4>
+                        <Table>
+                            <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Row>
+                    <Row>
+                        <h4>Species</h4>
+                        <Table>
+                            <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Row>
+                </Col>
+            </Row>
         </Container>
     }
 }
