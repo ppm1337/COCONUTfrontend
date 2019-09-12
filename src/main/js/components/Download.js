ab23fd7d85caa509a4dbdcd48b8dfece8223a9ec
Peter <peter.merseburger@uni-jena.de>
2019-09-12 12:27:36 +0200

    Add Download component

---
+++ src/main/js/components/Download.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container'
+
+const React = require("react");
+
+
+class Download extends React.Component {
+    render() {
+        return <Container>
+            <h4>Nothing to see here at the moment...</h4>
+        </Container>;
+    }
+}
+
+export default Download
